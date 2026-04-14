33fe514470de5330aba8abbe0d33f74cc9fe64b7
Mbaduko <nsengiyumvaclement247@gmail.com>
2026-04-14 13:22:07 +0200

    feat: Middlewares

--- index.js
+++ index.js
@@ -20,35 +20,58 @@ const students = [];
 
 server.use(express.json())
 
-server.route("/").get((req, res, next) => {
+server.route("/").get(
+    (req, res, next) => {
     res.send("Hello tethjygyu")
-})
+    }
 
-server.post("/add", async (req, res, next)=> {
-    try{
+)
+
+server.post("/add", 
+    (req, res, next) => {
         const inputs = req.body;
-        const [results] = await connPool.execute("INSERT INTO student (name, age) VALUES(?, ?)", [inputs.name, inputs.age]);
-        res.json({
-            message: "Byaciyemo",
-            userId: results.insertId
-        });
-    } catch(error) {
+
+        if(!inputs.name || !inputs.age)
+            res.status(400).json({
+                message: "Name and age are required"
+            })
+        next();
+    },
+    async (req, res, next)=> {
+        try{
+            const inputs = req.body;
+            const [results] = await connPool.execute("INSERT INTO student (name, age) VALUES(?, ?)", [inputs.name, inputs.age]);
+            res.json({
+                message: "Byaciyemo",
+                userId: results.insertId
+            });
+        } catch(error) {
+            console.error(error);
+            res.json({
+                message: "Error adding new student",
+            })
+        }
+    }
+)
+
+server.put("/students/:id", async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const name = req.body.name;
+        
+        const [results] = await connPool.execute("UPDATE student SET name = ? WHERE id = ?", [name, id]);
+        console.log(results);
+        res.status(200).json({
+            message: "Birakora"
+        })
+        
+    } catch (error) {
         console.error(error);
         res.json({
-            message: "Error adding new student",
-        })
+            message:"Erro"
+        });
     }
-})
-
-server.put("/students/:name", (req, res, next) => {
-    const name = req.params.name;
-    students.forEach((student, index) => {
-        if (student.name === name)
-            students[index].age = req.body.age;
-        res.json({
-            message: "Student updated successfully",
-        })
-    });
+    const id = req.params.id;
 })
 
 server.get("/students", async (req, res, next) => {
